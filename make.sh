1d2542374b801566af9842dcb7f24363fddc1bc5
Marton Juhasz <m@juhaszmarton.hu>
2011-12-14 12:13:56 +0100

    make.sh -> own plugin forks as remote ssh

--- make.sh
+++ make.sh
@@ -6,6 +6,13 @@ vimdir=`pwd`
 git submodule update --init
 git submodule foreach git checkout master
 
+# add own github forks to plugins for pushing
+git --git-dir=bundle/DBGp_Remote_Debugger_Interface/.git remote -v add ssh git@github.com:juhasz/DBGp-Remote-Debugger-Interface.git
+git --git-dir=bundle/Dim/.git remote -v add ssh git@github.com:juhasz/Dim.git
+git --git-dir=bundle/drupal_snippets/.git remote -v add ssh git@github.com:juhasz/drupal-snippets.git
+git --git-dir=bundle/mmd_vim/.git remote -v add ssh git@github.com:juhasz/mmd_vim.git
+git --git-dir=bundle/vimLaTex/.git remote -v add ssh git@github.com:juhasz/vimLaTeX.git
+
 # create backup and undo directory
 mkdir undodir backupdir
 
