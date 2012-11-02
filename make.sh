d6c156a1df427294ad566f596a8c196782153eff
Marton Juhasz <m@juhaszmarton.hu>
2012-11-02 04:24:28 +0100

    make.sh updated

--- make.sh
+++ make.sh
@@ -9,7 +9,6 @@ git submodule foreach git checkout master
 # add own github forks to plugins for pushing
 git --git-dir=bundle/Dim/.git remote -v add ssh git@github.com:juhasz/Dim.git
 git --git-dir=bundle/drupal_snippets/.git remote -v add ssh git@github.com:juhasz/drupal-snippets.git
-git --git-dir=bundle/mmd_vim/.git remote -v add ssh git@github.com:juhasz/mmd_vim.git
 
 # create backup and undo directory
 mkdir undodir backupdir
@@ -18,10 +17,11 @@ mkdir undodir backupdir
 touch bundle/vim_markdown/doc/EmptyFileForFixPathogen.txt
 
 # create symlinks for vimrc, gvimrc and ctags config file
-rm ~/.ctags
+rm ~/.ctags ~/.janus
 ln -s $vimdir/vimrc ~/.vimrc.after
 ln -s $vimdir/vimrc.before ~/.vimrc.before
 ln -s $vimdir/gvimrc ~/.gvimrc.after
 ln -s $vimdir/ctags ~/.ctags
+ln -s $vimdir/bundle ~/.janus
 
 exit 0
